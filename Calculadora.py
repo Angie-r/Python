1a4c62cfda612ab16be1c71a2ddea57daa57dea9
Angela Romero <anjera27@gmail.com>
2022-01-24 13:43:13 -0600

    Inicio del programa para un restaurante

---
+++ Calculadora.py
@@ -0,0 +1,18 @@
+# mensaje de bienvenida
+print("Bienvenido")
+
+menu_principal = """
+Que deseas pedir de comer
+
+1. Desayuno
+2. Almuerzo
+3. Cena
+4. Refaccion 
+
+"""
+    
+option = input(menu_principal)
+
+if option == '1':
+    menu_desa =  
+
