ac4aae47d59c060760246b0a995353e102a636d9
Angie-r <84364247+Angie-r@users.noreply.github.com>
2022-06-24 15:05:10 -0600

    Update Ejer_cadenas.py
    Se agrega como comentario el link de los ejercicios de cadena.

--- Ejer_cadenas.py
+++ Ejer_cadenas.py
@@ -1,3 +1,6 @@
+# https://aprendeconalf.es/docencia/python/ejercicios/cadenas/ - se agrega link de los ejecicios
+
+
 # Pedir nombre y numero entero, imprimir el nombre las veces que se ingreso al inicio.
 nombre = input('Ingresa tu nombre')
 numero = int(input('Ingresa un numero entero '))
